3b660bb60f37138b0c8bec8ae290f2009c0ae052
behdokhttt <bkiafar@yahoo.com>
2023-04-12 11:19:49 -0400

    CheckPoint

--- Server/Controllers/UD/InstructorController.cs
+++ Server/Controllers/UD/InstructorController.cs
@@ -27,6 +27,7 @@ using System.Numerics;
 using DOOR.Shared.DTO;
 using DOOR.Shared.Utils;
 using DOOR.Server.Controllers.Common;
+using static Duende.IdentityServer.Models.IdentityResources;
 
 namespace DOOR.Server.Controllers.UD
 {
@@ -64,11 +65,14 @@ namespace DOOR.Server.Controllers.UD
         }
 
         [HttpGet]
-        [Route("GetInstructor/{_InstructorID}")]
-        public async Task<IActionResult> GetInstructor(int _InstructorID)
+        [Route("GetInstructor/{_SchoolID}/{_InstructorID}")]
+        public async Task<IActionResult> GetInstructor(int _SchoolID , int _InstructorID)
         {
             InstructorDTO? lst = await _context.Instructors
+
+                .Where(x => x.SchoolId == _SchoolID)
                 .Where(x => x.InstructorId == _InstructorID)
+
                 .Select(sp => new InstructorDTO
                 {
                     CreatedBy = sp.CreatedBy,
@@ -85,6 +89,138 @@ namespace DOOR.Server.Controllers.UD
                     Zip = sp.Zip
                 }).FirstOrDefaultAsync();
             return Ok(lst);
+        }
+
+
+        [HttpPost]
+        [Route("PostInstructor")]
+        public async Task<IActionResult> PostInstructor([FromBody] InstructorDTO _InstructorDTO)
+        {
+            try
+            {
+                Instructor c = await _context.Instructors
+                .Where(x => x.SchoolId == _InstructorDTO.SchoolId)
+                .Where(x => x.InstructorId == _InstructorDTO.InstructorId)
+
+                .FirstOrDefaultAsync();
+
+                if (c == null)
+                {
+                    c = new Instructor
+                    {
+
+                        Salutation = _InstructorDTO.Salutation,
+                        FirstName = _InstructorDTO.FirstName,
+                        LastName = _InstructorDTO.LastName,
+                        StreetAddress = _InstructorDTO.StreetAddress,
+                        Zip = _InstructorDTO.Zip,
+                        Phone = _InstructorDTO.Phone
+
+
+                    };
+                    _context.Instructors.Add(c);
+                    await _context.SaveChangesAsync();
+                }
+            }
+
+            catch (DbUpdateException Dex)
+            {
+                List<OraError> DBErrors = ErrorHandling.TryDecodeDbUpdateException(Dex, _OraTranslateMsgs);
+                return StatusCode(StatusCodes.Status417ExpectationFailed, Newtonsoft.Json.JsonConvert.SerializeObject(DBErrors));
+            }
+            catch (Exception ex)
+            {
+                _context.Database.RollbackTransaction();
+                List<OraError> errors = new List<OraError>();
+                errors.Add(new OraError(1, ex.Message.ToString()));
+                string ex_ser = Newtonsoft.Json.JsonConvert.SerializeObject(errors);
+                return StatusCode(StatusCodes.Status417ExpectationFailed, ex_ser);
+            }
+
+            return Ok();
+        }
+
+        [HttpPut]
+        [Route("PutInstructor")]
+        public async Task<IActionResult> PutInstructor([FromBody] InstructorDTO _InstructorDTO)
+        {
+            try
+            {
+                Instructor c = await _context.Instructors
+
+                .Where(x => x.SchoolId == _InstructorDTO.SchoolId)
+                .Where(x => x.InstructorId == _InstructorDTO.InstructorId)
+
+                .FirstOrDefaultAsync();
+
+                if (c != null)
+                {
+
+                    c.Salutation = _InstructorDTO.Salutation;
+                    c.FirstName = _InstructorDTO.FirstName;
+                    c.LastName = _InstructorDTO.LastName;
+                    c.StreetAddress = _InstructorDTO.StreetAddress;
+                    c.Zip = _InstructorDTO.Zip;
+                    c.Phone = _InstructorDTO.Phone;
+
+
+
+                    _context.Instructors.Update(c);
+                    await _context.SaveChangesAsync();
+                }
+            }
+
+            catch (DbUpdateException Dex)
+            {
+                List<OraError> DBErrors = ErrorHandling.TryDecodeDbUpdateException(Dex, _OraTranslateMsgs);
+                return StatusCode(StatusCodes.Status417ExpectationFailed, Newtonsoft.Json.JsonConvert.SerializeObject(DBErrors));
+            }
+            catch (Exception ex)
+            {
+                _context.Database.RollbackTransaction();
+                List<OraError> errors = new List<OraError>();
+                errors.Add(new OraError(1, ex.Message.ToString()));
+                string ex_ser = Newtonsoft.Json.JsonConvert.SerializeObject(errors);
+                return StatusCode(StatusCodes.Status417ExpectationFailed, ex_ser);
+            }
+
+            return Ok();
+        }
+        [HttpDelete]
+        [Route("DeleteInstructor/{_SchoolID}/{_InstructorID}")]
+        public async Task<IActionResult> DeleteInstructor(int _SchoolID, int _InstructorID)
+        {
+            try
+            {
+                Instructor c = await _context.Instructors
+
+                .Where(x => x.SchoolId == _SchoolID)
+                .Where(x => x.InstructorId == _InstructorID)
+
+                .FirstOrDefaultAsync();
+
+                if (c != null)
+                {
+                    _context.Instructors.Remove(c);
+                    await _context.SaveChangesAsync();
+                }
+            }
+
+            catch (DbUpdateException Dex)
+            {
+                List<OraError> DBErrors = ErrorHandling.TryDecodeDbUpdateException(Dex, _OraTranslateMsgs);
+                return StatusCode(StatusCodes.Status417ExpectationFailed, Newtonsoft.Json.JsonConvert.SerializeObject(DBErrors));
+            }
+            catch (Exception ex)
+            {
+                _context.Database.RollbackTransaction();
+                List<OraError> errors = new List<OraError>();
+                errors.Add(new OraError(1, ex.Message.ToString()));
+                string ex_ser = Newtonsoft.Json.JsonConvert.SerializeObject(errors);
+                return StatusCode(StatusCodes.Status417ExpectationFailed, ex_ser);
+            }
+
+            return Ok();
         }
 
 
